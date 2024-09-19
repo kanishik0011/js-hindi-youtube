8ffba9863a99af7114666b3cf62ad5b9632adeac
KANISHIK SAI KOSHIK <kanishksaikaushik@gmail.com>
2024-09-19 08:47:30 +0000

    conversionOfOperations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,27 @@
+let score="null"
+console.log(typeof (score));
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+//""33"= 33
+// "33abc"=>naN
+//true==1
+//false==0
+let isLoggedIn=""
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//boolean 1=true
+//false=0
+
+let someNumber=33
+let stringNumber =String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
+console.log(typeof someNumber)
+console.log(someNumber)
+console.log(score)
+///why string to number conversion is confusing ///////////////////////////////////////opeartions
+ let value=34
+ let negValue=-value
+ console.log(negValue)
+ console.log(value)
