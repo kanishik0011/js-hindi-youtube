af1b65a6b2fd5d3c228df287ef2134fb338a44e2
KANISHIK SAI KOSHIK <kanishksaikaushik@gmail.com>
2024-09-19 12:47:54 +0530

    Update 02_dataType.js

--- 01_basics/02_dataType.js
+++ 01_basics/02_dataType.js
@@ -1,2 +1,19 @@
 console.log(typeof "hitesh" )
 console.log(typeof undefined)
+//number =>2 to power 53
+//bignit
+//string
+//boolean
+// null=> standard value (reperesentes/empty set)
+//undefined=>
+let state= "null";
+let name="kansihk";
+let age="56";
+let isloggedIn=false/true
+alert(3+3)
+console.log("kanishk")
+//////////////////type os any datatypes
+console.log(typeof "kanishk")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
