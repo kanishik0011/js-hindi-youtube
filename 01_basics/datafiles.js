fc231da26f8edaa08b1323aae0e3a0eab4dfca9f
KANISHIK SAI KOSHIK <kanishksaikaushik@gmail.com>
2024-09-20 07:16:53 +0000

    datafiles

---
+++ 01_basics/datafiles.js
@@ -0,0 +1,26 @@
+//#primitive 
+// 7 types : bigint,string,number,,boolean,null, undefined ,symbol
+//non primitive or referece type
+// arrays ,object ,function
+const heros=["hanum","naagraj","doga"]
+ let myobj={
+    name:"kanishk",
+    age:22,
+}
+ const myFunction=Function(
+ console.log(typeof undeifined) )
+
+///++++memory 
+// stack(primitive,give copy) ,heap (non primitive,give reference)
+let myYoutubeName="kanishk"
+let anothername = "kanishksai"
+anothername="kaushikchaicode"
+console.log(myYoutubeName);
+console.log(anothername);
+  
+let user ={
+    email:"usergmail.com"
+    upi:"user@ybbl"
+}
+let userTwo=userone
+userTwo="usergmail.com"
