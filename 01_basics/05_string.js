cbc8b0b44e54427e34f32274b9ae89b79ac8eb44
KANISHIK SAI KOSHIK <kanishksaikaushik@gmail.com>
2024-09-20 07:48:01 +0000

    strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,17 @@
+const name ="kanishk"
+const repocount =50
+const email="abc@gmail.com"
+//console.log(name+repocount+email)
+console.log(`hello my name is ${name} and my repocount is ${repocount}`)
+
+const gameName=new String ('kansihk')
+console.log(gameName[0]);
+console.log(gameName._proto_);
+console.log(gameName.charAt(2));
+console.log(gameName.indexof('t'));
+const newString =gameName.substring(o,4)
+console.log(newString)
+console.log(newStringOne.trim())
+
+const url="https://kanihsk.com//kanishik@gmail.com"
+url.replace('%20',"-")
