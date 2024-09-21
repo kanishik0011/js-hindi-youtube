6034ffb85ad1c9b698b03858373aa00b5675bf6e
KANISHIK SAI KOSHIK <kanishksaikaushik@gmail.com>
2024-09-21 05:55:57 +0000

    numberormath

---
+++ 01_basics/06numberormath.js
@@ -0,0 +1,29 @@
+const score=400
+console.log(score)
+const balance =new Number(100)
+console.log(balance)
+console.log(balance.toString().length)
+console.log(balance.toFixed(1));
+const otherNumber=123.456
+console.log(otherNumber.toPrecision(4))
+const hundreds =100000000
+console.log(hundreds.toLocaleString('en-IN'));
+//
+//++++++++++++++++MATHS_++++++++
+console.log(Math);
+console.log(Math.abs(-4))
+console.log(Math.round(4.6))
+console.log(Math.ceil(3.2))
+console.log(Math.floor(4.9))
+//squareroot
+console.log(Math.min(4,6,5,7,))
+console.log(Math.max(4,6,5,7,))
+console.log(Math.sqrt(49))
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log(Math.random()*10
++1);
+const min=20
+const max=40
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
