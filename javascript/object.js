3911a79b42d9ba986648e3df653cefe059b59bbd
KANISHIK SAI KOSHIK <kanishksaikaushik@gmail.com>
2024-10-02 05:22:36 +0000

    objects

---
+++ javascript/object.js
@@ -0,0 +1,69 @@
+///object 
+//const tinderUser=new Object()
+//console.log(tinderUser)
+
+//constructot or using singletonobject
+const tinderUsers={}
+ tinderUsers.id="234abc";
+ tinderUsers.name="sai";
+ tinderUsers.isLoggedIN=false
+
+ tinderUsers.age="23";
+//console.log(tinderUsers);
+
+
+const regularUser={
+    email:"kansihik@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"kanishk",
+            lastName:"sai"
+
+        }
+    }
+}
+//console.log(regularUser. fullname.userfullname.lastName)
+const obje1={1:"a",2:"b"}
+const obje2={
+    3:"c",4:"d"}
+    const object3={5:"c",6:"d"}
+     //const obje3={obje1,obje2}
+      //const obje3=Object.assign({},obje1,obje2,object3)
+      const obje3={...obje1,...obje2}
+     //console.log(obje3)
+     const user=[{
+        id:"1",
+       email:"abs"
+     },
+     {
+        id:"2",
+       email:"cvd"
+     },
+     {
+        id:"3",
+       email:"agfs"
+     },
+     ]
+user[1].email
+//console.log(tinderUsers);
+//console.log(Object.keys(tinderUsers));
+//console.log(Object.values(tinderUsers));
+//console.log(Object.entries(tinderUsers));
+
+//console.log(tinderUsers.hasOwnProperty('isLoggedIn'))
+
+//api in objects//
+const course={
+    courresName:"js",
+    courseInInstructor:"kanihsk",
+    coursePrice:"9999",
+}
+
+//syntax
+//
+//const{course1}=course
+///api//
+("name":"jais")
+
+end {{{{{}}}}}
+console.log
