f3e1dceddeb4b131cee0b12afe1aa4a2fcbf3fa4
KANISHIK SAI KOSHIK <kanishksaikaushik@gmail.com>
2024-10-02 06:14:36 +0000

    functions

---
+++ javascript/function.js
@@ -0,0 +1,77 @@
+///functions///
+//console.log("k");
+//console.log("a");
+//console.log("n");
+//console.log("i");
+//console.log("s");
+//console.log("h");
+//console.log("k");
+
+
+function callMyName(){console.log("k");
+    console.log("a");
+    console.log("n");
+    console.log("i");
+    console.log("s");
+    console.log("h");
+    console.log("k");
+    
+
+}
+//callMyName()
+
+//function hiKanishk(){
+    //console.log("k");
+//console.log("a");
+//console.log("n");
+//console.log("i");
+//console.log("s");
+//console.log("h");
+//console.log("k");
+
+//}//
+//hiKanishk()
+
+//sum of two number//
+//function addTwoNumber(number1,number2){
+   // console.log(number1+number2)
+
+//}
+//addTwoNumber(5,6)
+//addTwoNumber(4,"7")
+//addTwoNumber("5","8")
+//addTwoNumber(7,"a")
+//function other type//
+//function addTwoNumber(number1,number2){
+   // let result=number1+number2
+   // return result
+    //console.log(number1+number2)
+
+//}//console.log(number1+number2)
+//result number1 +number2
+//let result numbe1+number2
+
+//how many wYAS T TAKE VALURE=
+
+//function loginUserMessage(username ="sam"){if(username===undefined);
+    //console.log("please enter a username")
+    
+  //  return`${username} just logged in`
+ //   
+//}
+ //console.log (loginUserMessage("kanishk"))
+ //console.log (loginUserMessage())
+//function with object//
+
+ function calculaateCartPrice(...num8){ 
+    return num8
+    
+ }
+ //console.log(calculaateCartPrice(500,900,90,333,333))
+ const user={
+    username:"anisk",price:190
+ }
+ function handleObject(anyobject){console.log( `username is ${anyobject.username} and price is ${anyobject.price}`)
+
+ }
+ handleObject(user:"sam")
